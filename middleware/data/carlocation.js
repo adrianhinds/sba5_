a9968034b9d0e01d51cfe4a9b6d9b325666f058b
Adrian Hinds <adrianhinds@adrians-mbp.mynetworksettings.com>
2024-09-09 23:06:30 -0400

    added carlocation.js

---
+++ middleware/data/carlocation.js
@@ -0,0 +1,14 @@
+const carLocation = [
+    { id: 1, Model: "Bmw", location: "Berlin, Germany" },
+    { id: 2, Model: "Volkswagon", location: "Berlin, Germany" },
+    { id: 3, Model: "Cadillac", location: "Los Angeles, America" },
+    { id: 4, Model: "Toyota", location: "Tokyo, Japan" },
+    { id: 5, Model: "Honda", location: "Tokyo, Japan" },
+    { id: 6, Model: "Mercedes",  location: "Berlin, Germany" },
+    { id: 7, Model: "Range Rover", location: "Chicago, America" },
+    { id: 8, Model: "Rolls Royce", location: "Chicago, America" },
+    { id: 9, Model: "Jeep", location: "Los Angeles, America" },
+    { id: 10, Model: "Nissan", location: "Tokyo, Japan" }
+  ];
+  
+  module.exports = carLocation;
