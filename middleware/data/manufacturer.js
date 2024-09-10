08d0b4d56618a872b9ad1912610d70854bde620a
Adrian Hinds <adrianhinds@adrians-mbp.mynetworksettings.com>
2024-09-09 22:58:49 -0400

    added manufacturer.js

---
+++ middleware/data/manufacturer.js
@@ -0,0 +1,14 @@
+const manufacturer = [
+    { id: 1, Model: "Bmw", carId: 1, modelId: 1, series: "7", origin: "Germany" },
+    { id: 2, Model: "Volkswagon", carId: 2, modelId: 2, series: "5", origin: "Germany" },
+    { id: 3, Model: "Cadillac", carId: 3, modelId: 3, series: "8", origin: "America" },
+    { id: 4, Model: "Toyota", carId: 4, modelId: 4, series: "7", origin: "Japan" },
+    { id: 5, Model: "Honda", carId: 5, modelId: 5, series: "7", origin: "Japan" },
+    { id: 6, Model: "Mercedes", carId: 6, modelId: 6, series: "4", origin: "Germany" },
+    { id: 7, Model: "Range Rover", carId: 7, modelId: 7, series: "7", origin: "America" },
+    { id: 8, Model: "Rolls Royce", carId: 8, modelId: 8, series: "4", origin: "America" },
+    { id: 9, Model: "Jeep", carId: 9, modelId: 9, series: "5", origin: "America" },
+    { id: 10, Model: "Nissan", carId: 10, modelId: 10, series: "7", origin: "Japan" }
+  ];
+  
+  module.exports = manufacturer;
